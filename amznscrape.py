4acfcb8f134371bc7c96ce3e5df82b4333cfc46f
teejl <Thomas.Lockwood@Utexas.edu>
2019-01-28 22:34:52 +0000

    finalizing the features of the program

--- amznscrape.py
+++ amznscrape.py
@@ -6,32 +6,29 @@
 # I aim to run this throughout the day on my Raspberry Pi and collect pricing
 # data on items that I am interested in. For fun.
 
+# in order to run this script please type the following
+# > python3 amznscrape.py "Data Science"
+# note: "Data Science" can be replaced with any item you would like to scrape amazon with
+
+
 # import packages
 from bs4 import BeautifulSoup
 import requests
 import re
 import pandas as pd
 import time
+import sys
 
-# establish variables
-# site = "http://www.example.com"
-# site = "https://www.amazon.com"
-# site = "https://www.pandora.com"
-
-# simple example
-
-
+# tested with below parameters
 search_word = "Data Science"
-
-# boss battle 
 site = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search--alias%3Daps&field-keywords=Data+Science&rh=i%3Aaps%2Ck%3AData+Science"
-#site = 'https://www.amazon.com/s/ref=sr_pg_3?rh=i%3Aaps%2Ck%3AData+Science&page=3&keywords=Data+Science&ie=UTF8&qid=1548565321'
-# then hypothetically it would repeat for the rest of the 20 pages
-# program 1 run here input: link, dataframe; output: dataframe
+site = 'https://www.amazon.com/s/ref=sr_pg_3?rh=i%3Aaps%2Ck%3AData+Science&page=3&keywords=Data+Science&ie=UTF8&qid=1548565321'
+
+
 def scrape_amzn(search_word, page=1, df=pd.DataFrame(columns = ['Name', 'Price', 'Link']), tries=3):
 	'''
 	input: search_word, page, dataframe
-	output: dataframe
+	output: dataframe [Name, Price, Link] of amzn search
 	'''
 	# establish link
 	if not page-1:
@@ -67,7 +64,7 @@ def scrape_amzn(search_word, page=1, df=pd.DataFrame(columns = ['Name', 'Price',
 					,'Link':line['href']}, ignore_index=True) # update the link
 	# print off data frame
 	if len(df_old.index) == len(df.index):
-		if tries:
+		if tries: # retry on error (sometimes connection ends recursion)
 			print('Something went wrong! Retrying ' + str(tries) + ' more times...')
 			return(scrape_amzn(search_word,page,df, tries-1))
 		else:
@@ -78,7 +75,7 @@ def scrape_amzn(search_word, page=1, df=pd.DataFrame(columns = ['Name', 'Price',
 		return(scrape_amzn(search_word, page+1, df))
 
 # establish search word
-search_word = "Data Science"
+search_word = sys.argv[1]
 final = scrape_amzn(search_word)
 print(final)
 
